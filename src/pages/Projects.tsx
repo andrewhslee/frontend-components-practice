1402c114a665270d9cf5f05c6109c776339cc0ca
Han Se Lee <hanselee@Hans-MBP.attlocal.net>
2023-10-30 13:40:34 -0500

    first commit

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Projects: FC = () => {
+    return (
+        <div>
+            These are my projects!
+        </div>
+    )
+}
+
+export default Projects;
